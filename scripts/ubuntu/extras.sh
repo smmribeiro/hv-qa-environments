549554b6b4393cea804d89686c8e6f5095a2904d
Jean Paul Ruiz <Jean Paul Ruiz jeanpaul.ruiz@hitachivantara.com>
2019-08-19 15:56:48 -0400

    adding instructions to install zip and unzip in ubuntu

--- scripts/ubuntu/extras.sh
+++ scripts/ubuntu/extras.sh
@@ -3,3 +3,6 @@ echo "installing extra packages"
 sudo apt-get install -y firefox > /dev/null 2>&1
 sudo apt-get install -y gedit > /dev/null 2>&1
 sudo apt-get install -y libwebkitgtk-1.0-0 > /dev/null 2>&1
+
+sudo apt-get install -y zip > /dev/null 2>&1
+sudo apt-get install -y unzip > /dev/null 2>&1
