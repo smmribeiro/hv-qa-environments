bff384a292c160c4c7561865de821adc5309ee9d
Jean Paul Ruiz <jpruiz114@gmail.com>
2019-10-30 00:23:40 -0400

    fixing empty variable validation

--- scripts/ubuntu/extras.sh
+++ scripts/ubuntu/extras.sh
@@ -4,8 +4,8 @@ echo "installing extra packages"
 
 UBUNTU_VERSION=$1
 
-if [ $UBUNTU_VERSION -eq 0 ]; then
-  $UBUNTU_VERSION=16.04
+if [ -z "$UBUNTU_VERSION" ]; then
+  $UBUNTU_VERSION="16.04"
 fi
 
 sudo apt-get install -y firefox > /dev/null 2>&1
