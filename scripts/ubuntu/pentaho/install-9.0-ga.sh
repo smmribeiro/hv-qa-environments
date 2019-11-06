6ccb06552bacb61b0d8db8267cba5d541ca65e87
Jean Paul Ruiz <Jean Paul Ruiz jeanpaul.ruiz@hitachivantara.com>
2019-11-06 11:22:10 -0500

    using latest 9.0 ga version

--- scripts/ubuntu/pentaho/install-9.0-ga.sh
+++ scripts/ubuntu/pentaho/install-9.0-ga.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=/host/builds/business-analytics/pentaho-business-analytics-9.0.0.0-259-x64.bin
+FILE=/host/builds/business-analytics/pentaho-business-analytics-9.0.0.0-289-x64.bin
 BUILD_HOST=http://build.pentaho.net/hosted
 
 if [ -f "$FILE" ]; then
@@ -11,7 +11,7 @@ else
   if curl --head --silent --fail $BUILD_HOST 2> /dev/null; then
     echo "Could connect to $BUILD_HOST, moving on"
 
-    curl $BUILD_HOST/9.0-QAT/259/pentaho-business-analytics-9.0.0.0-259-x64.bin -o $FILE
+    curl $BUILD_HOST/9.0-QAT/289/pentaho-business-analytics-9.0.0.0-289-x64.bin -o $FILE
   else
     echo "Could not connect to $BUILD_HOST, check your network connection and try again"
     exit 1
@@ -19,4 +19,4 @@ else
 fi
 
 echo "installing 9.0 no questions"
-printf '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\ny\n1\n\n1234567890\n1234567890\n\nn\nn\n' | /host/builds/business-analytics/pentaho-business-analytics-9.0.0.0-259-x64.bin
+printf '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\ny\n1\n\n1234567890\n1234567890\n\nn\nn\n' | /host/builds/business-analytics/pentaho-business-analytics-9.0.0.0-289-x64.bin
