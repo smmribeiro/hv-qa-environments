78e1ddcdb3c32b196228f91229c5a84177ecc1ba
Jean Paul Ruiz <jpruiz114@gmail.com>
2020-02-28 23:54:52 -0500

    Updating PHP 7 script

--- scripts/ubuntu/install-php-7.4.sh
+++ scripts/ubuntu/install-php-7.4.sh
@@ -19,3 +19,25 @@ php -v
 
 # Install PHP 7.4 Extensions
 sudo apt install php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl -y
+
+# Install Xdebug
+
+sudo apt-get install php-xdebug
+
+sudo phpdismod xdebug # Disable xdebug
+php --version | grep Xdebug
+
+sudo phpenmod xdebug # Enable xdebug
+php --version | grep Xdebug
+
+sudo service apache2 restart
+
+more /etc/php/7.4/mods-available/xdebug.ini
+
+# zend_extension=xdebug.so
+
+# xdebug.idekey=PHPSTORM
+# xdebug.remote_enable=true
+# xdebug.remote_host=192.168.0.111
+# xdebug.remote_port=9000
+# xdebug.show_error_trace=1
