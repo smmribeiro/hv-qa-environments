2dc754352b74da701feb1ae4c8db8052acefd153
Jean Paul Ruiz <Jean Paul Ruiz jeanpaul.ruiz@hitachivantara.com>
2020-03-24 11:41:56 -0400

    Adding Ubuntu version validation for R installer

--- scripts/ubuntu/install-r.sh
+++ scripts/ubuntu/install-r.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-# How to Install R on Ubuntu 18.04
-# https://linuxize.com/post/how-to-install-r-on-ubuntu-18-04/
-
 # Install the packages necessary to add a new repository over HTTPS:
 sudo apt install -y apt-transport-https software-properties-common
 
 # Enable the CRAN repository and add the CRAN GPG key to your system using the following commands:
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+
+version=$(lsb_release -r -s)
+
+if [ -z "$1" ]; then
+  version=$(lsb_release -r -s)
+else
+  version=$1
+fi
+
+echo "installing R for ubuntu $version"
+
+if [ "$version" == "16.04" ] ; then
+  sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/'
+fi
+
+if [ "$version" == "18.04" ] ; then
+  sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+fi
 
 # Now that the apt repository is added, update the packages list and install the R package by typing:
 sudo apt update
