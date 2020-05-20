8a534945948b6debc4d9d55f412bd8fb71251c2a
Jean Paul Ruiz <JeanPaul.Ruiz@hitachivantara.com>
2020-05-20 19:39:53 -0400

    Adding changes for karaf ssh connection to pdi and pme

--- scripts/ubuntu/pentaho/enable-karaf-console.sh
+++ scripts/ubuntu/pentaho/enable-karaf-console.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
 STRING="ssh"
-FILE="/home/vagrant/Pentaho/design-tools/data-integration/system/karaf/etc/org.apache.karaf.features.cfg"
-ls -al $FILE
 
-if [ ! -z $(grep "$STRING" "$FILE") ]; then
-  echo "karaf console already enabled"
+DI_FILE="/home/vagrant/Pentaho/design-tools/data-integration/system/karaf/etc/org.apache.karaf.features.cfg"
+ls -al $DI_FILE
+
+if [ ! -z $(grep "$STRING" "$DI_FILE") ]; then
+  echo "karaf console already enabled for Data Integration"
 else
-  echo "karaf console not enabled yet"
-  cp /host/scripts/ubuntu/pentaho/org.apache.karaf.features.cfg $FILE
-  echo "karaf features config file updated, karaf console enabled"
+  echo "karaf console not enabled yet for Data Integration"
+  cp /host/scripts/ubuntu/pentaho/pdi-org.apache.karaf.features.cfg $DI_FILE
+  echo "karaf features config file updated, karaf console enabled for Data Integration"
+fi
+
+ME_FILE="/home/vagrant/Pentaho/design-tools/metadata-editor/system/karaf/etc/org.apache.karaf.features.cfg"
+ls -al $ME_FILE
+
+if [ ! -z $(grep "$STRING" "$ME_FILE") ]; then
+  echo "karaf console already enabled for Metadata Editor"
+else
+  echo "karaf console not enabled yet for Metadata Editor"
+  cp /host/scripts/ubuntu/pentaho/pme-org.apache.karaf.features.cfg $ME_FILE
+  echo "karaf features config file updated, karaf console enabled for Metadata Editor"
 fi
 
 # delete Karaf caches
 rm -rf /home/vagrant/Pentaho/design-tools/data-integration/system/karaf/caches
-rm -rf /home/vagrant/Pentaho/server/pentaho-server/pentaho-solutions/system/karaf/caches
+rm -rf /home/vagrant/Pentaho/design-tools/metadata-editor/system/karaf/caches
 
+# vim /home/vagrant/.ssh/known_hosts
+# delete entry
 # ssh -p 8802 karaf@localhost -oHostKeyAlgorithms=+ssh-dss
 # use "karaf" as password
