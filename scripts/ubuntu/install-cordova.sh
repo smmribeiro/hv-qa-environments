7e8b06867d74b2c6dc8aed9a1215a4e4a9760d38
Jean Paul Ruiz <jpruiz114@gmail.com>
2020-05-20 14:57:57 -0400

    Adding scripts to install NodeJS, npm and Cordova

---
+++ scripts/ubuntu/install-cordova.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo npm install -g cordova
---
+++ scripts/ubuntu/install-nodejs-and-npm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y nodejs
+nodejs -v
+
+sudo apt-get install -y libssl1.0-dev
+sudo apt-get install -y nodejs-dev
+sudo apt-get install -y node-gyp
+sudo apt-get install -y npm
+npm -v
