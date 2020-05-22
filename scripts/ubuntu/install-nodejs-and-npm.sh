ba5631ecc996d0a24caaa370b0a690b668301d0b
Jean Paul Ruiz <jpruiz114@gmail.com>
2020-05-22 17:38:51 -0400

    Adding scripts to install nodejs, npm and changes to the PHP 7 one

--- scripts/ubuntu/install-nodejs-and-npm.sh
+++ scripts/ubuntu/install-nodejs-and-npm.sh
@@ -10,3 +10,9 @@ sudo apt-get install -y nodejs-dev
 sudo apt-get install -y node-gyp
 sudo apt-get install -y npm
 npm -v
+
+sudo npm install -g cordova
+
+sudo apt-get install -y gradle
+
+yes | /home/vagrant/Android/Sdk/tools/bin/sdkmanager --licenses && yes | sdkmanager --update
--- scripts/ubuntu/install-php-7.4.sh
+++ scripts/ubuntu/install-php-7.4.sh
@@ -18,13 +18,13 @@ sudo apt install php7.4
 php -v
 
 # Install PHP 7.4 Extensions
-sudo apt install php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl -y
+sudo apt install -y php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl
 
 sudo apt-get install -y php7.4-sqlite
 
 # Install Xdebug
 
-sudo apt-get install php-xdebug
+sudo apt-get install -y php-xdebug
 
 sudo phpdismod xdebug # Disable xdebug
 php --version | grep Xdebug
