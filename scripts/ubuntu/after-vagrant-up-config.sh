78638847b0ad05ea1bca54a4ec511ee802e34d99
Jean Paul Ruiz <jpruiz114@gmail.com>
2020-06-10 17:57:51 -0400

    Adding changes to setup xdebug config

--- scripts/ubuntu/after-vagrant-up-config.sh
+++ scripts/ubuntu/after-vagrant-up-config.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "after vagrant up config"
+echo "###########################"
+echo "# after vagrant up config #"
+echo "###########################"
+
+STRING="export XDEBUG_CONFIG=\"idekey=PHPSTORM\""
+FILE=/home/vagrant/.profile
+
+if  grep -q "$STRING" "$FILE" ; then
+  echo "Xdebug PHPStorm IDE Key already in profile file"
+else
+  echo "Xdebug PHPStorm IDE Key not found in profile file"
+  echo $STRING >> $FILE
+  echo "Xdebug PHPStorm IDE Key added to profile file"
+fi
--- scripts/ubuntu/get-host-ip.sh
+++ scripts/ubuntu/get-host-ip.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo "get host ip"
+echo "###############"
+echo "# get host ip #"
+echo "###############"
 
 # How to remove CTRL-M (^M) characters from a file in Linux
 sed -e "s/\r//g" /host/shared/host-ip.txt > /host/shared/host-ip-no-ctrl-m.txt
 
 IP=$(cat /host/shared/host-ip-no-ctrl-m.txt)
-echo "$IP"
+echo "The host IP address is: $IP"
 
 sudo sed -i "/xdebug.remote_host=/c\xdebug.remote_host=$IP" /etc/php/7.4/mods-available/xdebug.ini
-more /etc/php/7.4/mods-available/xdebug.ini
+#more /etc/php/7.4/mods-available/xdebug.ini
 
 sudo service apache2 restart
