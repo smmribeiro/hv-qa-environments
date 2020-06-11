db2b02dff4617b81b3e5e4a2ee541c85e122f0e2
Jean Paul Ruiz <JeanPaul.Ruiz@hitachivantara.com>
2020-06-11 15:14:22 -0400

    Adding IA env variables

--- scripts/ubuntu/after-vagrant-up-config.sh
+++ scripts/ubuntu/after-vagrant-up-config.sh
@@ -13,3 +13,25 @@ else
   echo $STRING >> $FILE
   echo "Xdebug PHPStorm IDE Key added to profile file"
 fi
+
+STRING="export IA_HOME=\"/home/vagrant/InstallAnywhere 2020\""
+FILE=/home/vagrant/.profile
+
+if  grep -q "$STRING" "$FILE" ; then
+  echo "IA Home already in profile file"
+else
+  echo "IA Home not found in profile file"
+  echo $STRING >> $FILE
+  echo "IA Home added to profile file"
+fi
+
+STRING="export IA_PATH_INSTALL_PROJECT_PATH=\"/host/shared/pentaho-installer/installer-tool\""
+FILE=/home/vagrant/.profile
+
+if  grep -q "$STRING" "$FILE" ; then
+  echo "IA Install Project Path already in profile file"
+else
+  echo "IA Install Project Path not found in profile file"
+  echo $STRING >> $FILE
+  echo "IA Install Project Path added to profile file"
+fi
